4f8e17a544a9a515636dbb84d3f16f6ee607d07d
Kabalan Elahmadie <kahmadieh@yahoo.com>
2019-05-15 17:19:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,3 @@
 function happyHolidays(){
   return "Happy holidays!";
 }
-function happyHolidaysTo(name){
-  return 'Happy holidays, ' + name + '!';
-}
-function happyHolidaysTo(name, holiday){
-  return "Happy " + holiday + ", " + name + "!";
-}
-function holidayCountDown(days, holiday){
-  return "It's " + days + "days until " + holiday "!";
-}
